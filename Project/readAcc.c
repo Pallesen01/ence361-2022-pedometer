991cfdf1399fe3ed4ea8b12ef5e492a4ab809996
Ben Stewrt <bst72@uclive.ac.nz>
2022-03-18 17:23:33 +1300

    Set up project, added ability to cycle between units

---
+++ Project/readAcc.c
@@ -0,0 +1,231 @@
+/**********************************************************
+ *
+ * readAcc.c
+ *
+ * Reads acceleration in three dimensions and
+ * displays the resulting data on the Orbit OLED display.
+ * The data can be displayed in 3 different units which can
+ * be changed using the "UP" button.
+ *
+ *    Ben Stewart and Daniel Pallesen
+ *    18th of March 2022
+ *
+ **********************************************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_i2c.h"
+#include "driverlib/pin_map.h" //Needed for pin configure
+#include "driverlib/systick.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/i2c.h"
+#include "../OrbitOLED/OrbitOLEDInterface.h"
+#include "utils/ustdlib.h"
+#include "acc.h"
+#include "i2c_driver.h"
+#include "buttons4.h"
+
+typedef struct{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} vector3_t;
+
+/**********************************************************
+ * Constants
+ **********************************************************/
+// Systick configuration
+#define SYSTICK_RATE_HZ    10
+
+/*******************************************
+ *      Local prototypes
+ *******************************************/
+void initClock (void);
+void initDisplay (void);
+void displayUpdate (char *str1, char *str2, int16_t num, uint8_t charLine);
+void initAccl (void);
+vector3_t getAcclData (void);
+
+/***********************************************************
+ * Initialisation functions: clock, SysTick, PWM
+ ***********************************************************
+ * Clock
+ ***********************************************************/
+void
+initClock (void)
+{
+    // Set the clock rate to 20 MHz
+    SysCtlClockSet (SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+}
+
+/*********************************************************
+ * initDisplay
+ *********************************************************/
+void
+initDisplay (void)
+{
+    // Initialise the Orbit OLED display
+    OLEDInitialise ();
+}
+
+//*****************************************************************************
+// Function to display a changing message on the display.
+// The display has 4 rows of 16 characters, with 0, 0 at top left.
+//*****************************************************************************
+void
+displayUpdate (char *str1, char *str2, int16_t num, uint8_t charLine)
+{
+    char text_buffer[17];           //Display fits 16 characters wide.
+
+    // "Undraw" the previous contents of the line to be updated.
+    OLEDStringDraw ("                ", 0, charLine);
+    // Form a new string for the line.  The maximum width specified for the
+    //  number field ensures it is displayed right justified.
+    usnprintf(text_buffer, sizeof(text_buffer), "%s %s %3d", str1, str2, num);
+    // Update line on display.
+    OLEDStringDraw (text_buffer, 0, charLine);
+}
+
+/*********************************************************
+ * initAccl
+ *********************************************************/
+void
+initAccl (void)
+{
+    char    toAccl[] = {0, 0};  // parameter, value
+
+    /*
+     * Enable I2C Peripheral
+     */
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
+
+    /*
+     * Set I2C GPIO pins
+     */
+    GPIOPinTypeI2C(I2CSDAPort, I2CSDA_PIN);
+    GPIOPinTypeI2CSCL(I2CSCLPort, I2CSCL_PIN);
+    GPIOPinConfigure(I2CSCL);
+    GPIOPinConfigure(I2CSDA);
+
+    /*
+     * Setup I2C
+     */
+    I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), true);
+
+    GPIOPinTypeGPIOInput(ACCL_INT2Port, ACCL_INT2);
+
+    //Initialize ADXL345 Acceleromter
+
+    // set +-2g, 13 bit resolution, active low interrupts
+    toAccl[0] = ACCL_DATA_FORMAT;
+    toAccl[1] = (ACCL_RANGE_2G | ACCL_FULL_RES);
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+
+    toAccl[0] = ACCL_PWR_CTL;
+    toAccl[1] = ACCL_MEASURE;
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+
+
+    toAccl[0] = ACCL_BW_RATE;
+    toAccl[1] = ACCL_RATE_100HZ;
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+
+    toAccl[0] = ACCL_INT;
+    toAccl[1] = 0x00;       // Disable interrupts from accelerometer.
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+
+    toAccl[0] = ACCL_OFFSET_X;
+    toAccl[1] = 0x00;
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+
+    toAccl[0] = ACCL_OFFSET_Y;
+    toAccl[1] = 0x00;
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+
+    toAccl[0] = ACCL_OFFSET_Z;
+    toAccl[1] = 0x00;
+    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+}
+
+/********************************************************
+ * Function to read accelerometer
+ ********************************************************/
+vector3_t
+getAcclData (void)
+{
+    char    fromAccl[] = {0, 0, 0, 0, 0, 0, 0}; // starting address, placeholders for data to be read.
+    vector3_t acceleration;
+    uint8_t bytesToRead = 6;
+
+    fromAccl[0] = ACCL_DATA_X0;
+    I2CGenTransmit(fromAccl, bytesToRead, READ, ACCL_ADDR);
+
+    acceleration.x = (fromAccl[2] << 8) | fromAccl[1]; // Return 16-bit acceleration readings.
+    acceleration.y = (fromAccl[4] << 8) | fromAccl[3];
+    acceleration.z = (fromAccl[6] << 8) | fromAccl[5];
+
+    return acceleration;
+}
+
+
+/********************************************************
+ * main
+ ********************************************************/
+int
+main (void)
+{
+    vector3_t acceleration_raw;
+    uint8_t unitState = 0; //Initially display Raw units
+
+    initClock ();
+    initAccl ();
+    initDisplay ();
+    initButtons ();
+
+    OLEDStringDraw ("Accelerometer", 0, 0);
+
+    while (1)
+    {
+        SysCtlDelay (SysCtlClockGet () / 6);    // Approx 2 Hz
+        acceleration_raw = getAcclData();
+
+        uint8_t butState;
+        updateButtons ();
+
+        butState = checkButton (UP); //Gets the current state of the 'UP button
+
+        if (butState == PUSHED) { //Checks if the 'UP' button has been pushed
+            unitState ++; //Changes the units
+            if (unitState == 3) {
+                //Checks to see if the variable has gone out of range as only 0, 1 and 2
+                //are valid unitState values. More units could be added in future.
+                unitState = 0;
+            }
+        }
+
+        if (unitState == 0) {
+            //Display units = raw
+            displayUpdate ("Accl", "X", acceleration_raw.x, 1);
+            displayUpdate ("Accl", "Y", acceleration_raw.y, 2); //Put this code in a function to limit repeated code
+            displayUpdate ("Accl", "Z", acceleration_raw.z, 3);
+        } else if (unitState == 1) {
+            //Display units = g
+            displayUpdate ("Accl", "X", acceleration_raw.x / 256, 1); //Turn the magic numbers into constants
+            displayUpdate ("Accl", "Y", acceleration_raw.y / 256, 2);
+            displayUpdate ("Accl", "Z", acceleration_raw.z / 256, 3);
+        } else {
+            //Display units = ms^-2
+            displayUpdate ("Accl", "X", (acceleration_raw.x * 9.81) / 256, 1);
+            displayUpdate ("Accl", "Y", (acceleration_raw.y * 9.81) / 256, 2);
+            displayUpdate ("Accl", "Z", (acceleration_raw.z * 9.81) / 256, 3);
+        }
+    }
+}
